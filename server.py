759a0592771b7c72cbe9249e92ba4d2602aed242
blade <flyblade@gmail.com>
2013-03-27 08:54:17 -0700

    Refact

--- server.py
+++ server.py
@@ -13,59 +13,7 @@ import urllib
 from urlparse import urlparse
 from threading import Thread
 from Queue import Queue
-
-websites = {
-    "qq": {
-        "title": "腾讯视频(flv)",
-        "url": "http://v.qq.com",
-        "parser": QQWebParser,
-        "icon": "http://v.qq.com/favicon.ico",
-        "info": "flv格式，不分段，但不能选择清晰度。"
-    },
-    "qqmp4": {
-        "title": "腾讯视频(mp4)",
-        "url": "http://v.qq.com",
-        "parser": QQWebParserMP4,
-        "icon": "http://v.qq.com/favicon.ico",
-        "info": "mp4格式，分段，可选择清晰度。"
-    },
-    "youku": {
-        "title": "优酷视频",
-        "url": "http://www.youku.com",
-        "parser": YoukuWebParser,
-        "icon": "http://www.youku.com/favicon.ico",
-        "info": "mp4格式，分段，可选择清晰度。"
-    },
-    "wangyi": {
-        "title": "网易公开课",
-        "url": "http://open.163.com",
-        "parser": WangyiWebParser,
-        "icon": "http://open.163.com/favicon.ico",
-        "info": "Nothing"
-    },
-    "yinyuetai": {
-        "title": "音悦台",
-        "url": "http://www.yinyuetai.com",
-        "parser": YinyuetaiWebParser,
-        "icon": "http://s.yytcdn.com/favicon.ico",
-        "info": "Nothing"
-    },
-    "kankan": {
-        "title": "迅雷看看",
-        "url": "http://www.kankan.com",
-        "parser": KankanWebParser,
-        "icon": "http://www.kankan.com/favicon.ico",
-        "info": "Nothing",
-        "externaldownload": True
-    },
-    "youtube": {
-        "title": "Youtube",
-        "url": "http://www.youtube.com",
-        "parser": YoutubeWebParser,
-        "icon": "http://www.youtube.com/favicon.ico",
-        "info": "Nothing"
-    },
-}
+from Contents import *
 
 current_website = None
 currentVideo = None
@@ -76,63 +24,6 @@ player = None
 downloader = None
 playThread = None
 playQueue = Queue()
-
-experiment = True
-
-actionToKey = {
-    'pause': 'p',
-    'stop': 'q',
-    'volup': '+',
-    'voldown': '-',
-    'f30': '\x1B[D',
-    'b30': '\x1B[C',
-    'f600': '\x1B[A',
-    'b600': '\x1B[B',
-    'showinfo': 'z',
-    'speedup': '1',
-    'speeddown': '2',
-    'togglesub': 's',
-}
-
-actionDesc = [
-    [
-        ('pause', 'Pause'),
-        ('stop', 'Stop'),
-        ('volup', 'Increase Volume'),
-        ('voldown', 'Decrease Volume')
-    ],
-    # [
-    #     ('f30', 'Seek +30'),
-    #     ('b30', 'Seek -30'),
-    #     ('f600', 'Seek +600'),
-    #     ('b600', 'Seek -600')
-    # ],
-    # [
-    #     ('showinfo', 'z'),
-    #     ('speedup', '1'),
-    #     ('speeddown', '2'),
-    #     ('togglesub', 's'),
-    # ]
-]
-
-actionToKeyMac = {
-    'MPlayerX':
-    {
-        'pause': '49',
-        'stop': '12 using command down',
-        'volup': '24',
-        'voldown': '27',
-        'fullscreen': '3',
-    },
-    'VLC':
-    {
-        'pause': '49',
-        'stop': '12 using command down',
-        'volup': '126 using command down',
-        'voldown': '125 using command down',
-        'fullscreen': '3 using command down',
-    }
-}
 
 import logging
 logging.basicConfig(format='%(asctime)s %(module)s %(levelname)s: %(message)s',
@@ -239,7 +130,7 @@ def play_url(where=0):
         else:
             # Because omxplayer doesn't support list we have to play one by one.
             sections = parseM3U()
-            if experiment and current_website == 'youku' and len(sections) > 1:
+            if 'merge' in websites[current_website] and websites[current_website]['merge'] and len(currentVideo.sections) > 1:
                 logging.info("Experiment to merge video for youku")
                 outputFileName = 'all.ts'
                 exec_filename = "merge.sh"
@@ -248,7 +139,7 @@ def play_url(where=0):
                     os.remove(outputFileName)
                 except OSError:
                     pass
-                lines = []
+                lines = ["#%s" % currentVideo.url]
                 p_list = []
                 for idx, v in enumerate(sections):
                     pname = "p%s" % idx
