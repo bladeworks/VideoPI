18c0d62b159e6e3807a70f2422f7b7188f6ab4be
blade <flyblade@gmail.com>
2013-06-04 10:02:24 -0700

    Test

--- server.py
+++ server.py
@@ -448,7 +448,7 @@ def restart():
 @error(404)
 def error404(error):
     logging.debug("404 on url: %s", request.url)
-    if url == "http://192.168.1.100/jsonrpc":
+    if request.url == "http://192.168.1.100/jsonrpc":
         return
     p_results = urlparse(request.url)
     to_replace = p_results.scheme + "://" + p_results.netloc
