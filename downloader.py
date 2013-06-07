720e6436bd3d176905486f9d8fdee60243b08796
blade <flyblade@gmail.com>
2013-06-06 20:42:36 -0700

    Test

--- downloader.py
+++ downloader.py
@@ -97,6 +97,7 @@ class Downloader:
             try:
                 conn.request("GET", self.path, None, headers)
                 resp = conn.getresponse()
+                logging.info("resp status = %s", resp.status)
                 if resp.status == 200:
                     self.result_queue.put({part_num: resp.read()})
                     break
