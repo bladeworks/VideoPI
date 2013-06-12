38400477540264f6ec0ce6b6bb69428d243e8b13
blade <flyblade@gmail.com>
2013-06-12 10:12:43 -0700

    Test

--- Helper.py
+++ Helper.py
@@ -2,17 +2,18 @@
 # -*- coding: utf8 -*-
 
 import os
+import logging
 
 
 def newFifo(filename):
     try:
         os.mkfifo(filename)
     except OSError:
-        pass
+        logging.exception("Got exception when newFifo")
 
 
 def newDir(dirname):
     try:
         os.mkdir(dirname)
     except OSError:
-        pass
+        logging.exception("Got exception when newDir")
