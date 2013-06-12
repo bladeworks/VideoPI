ed9adb40023844455cbe03acdfaa5c99112e438c
blade <flyblade@gmail.com>
2013-06-12 12:57:52 -0700

    Test

--- Helper.py
+++ Helper.py
@@ -2,18 +2,17 @@
 # -*- coding: utf8 -*-
 
 import os
-import logging
 
 
 def newFifo(filename):
     try:
         os.mkfifo(filename)
     except OSError:
-        logging.exception("Got exception when newFifo")
+        pass
 
 
 def newDir(dirname):
     try:
         os.mkdir(dirname)
     except OSError:
-        logging.exception("Got exception when newDir")
+        pass
--- downloader.py
+++ downloader.py
@@ -115,7 +115,7 @@ class Downloader:
                     filesize += len(v)
                 assert filesize > (self.current_step_size - 1) * self.chunk_size
                 logging.debug("The avg speed is %s", self.computeSpeed(filesize, (end_time - self.start_time)))
-                self.write_queue.put(''.join(result.values))
+                self.write_queue.put(''.join(result.values()))
                     # logging.debug("Begin write file")
                     # filename = "/tmp/download_part"
                     # with open(filename, 'a+b') as f:
